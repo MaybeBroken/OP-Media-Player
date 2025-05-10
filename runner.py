96635602e4748b11bea15bae8db3a45377454685
MaybeBroken <davidsponseller123@gmail.com>
2025-05-09 18:29:11 -0700

    update path loading

--- runner.py
+++ runner.py
@@ -24,13 +24,8 @@ while not is_python_installed:
     print("Waiting for Python to be installed...")
     is_python_installed = os.system("python3 --version") == 0
     sleep(5)
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-# execute the Main.py script in the current directory
-
-# Execute the updater.py script and wait for it to finish
 os.system(
-    'python3 updater.py --name "OP Media Player" --version 0.0.1 --file-index-path remove_index.json'
+    'python3 updater.py --name "OP Media Player" --version 0.0.1 --file-index-path ./remove_index.json'
 )
-
 os.system("python3 Main.py")
