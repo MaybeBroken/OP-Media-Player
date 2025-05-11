49e27c1afaf2d8a4d6b26f4e2fbc0ae2b482141e
MaybeBroken <davidsponseller123@gmail.com>
2025-05-10 18:54:04 -0600

    add codesigning

---
+++ CodeSign.bat
@@ -0,0 +1,3 @@
+signtool.exe sign /a /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 /v /d "MaybeBroken" ./"OP Media Player Installer.exe"
+signtool.exe sign /a /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 /v /d "MaybeBroken" ./"OP Media Player.exe"
+pause
